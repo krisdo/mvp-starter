525c47e027924136205452befbfe34cc94ea7e17
Krissy Do <krissytdo@hotmail.com>
2020-05-30 10:26:29 -0700

    mongoose

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -137,7 +137,7 @@ var addResults = (results) =>{
       advice: results.advice,
       url: results.url};
 
-    Battle.findOneAndUpdate({},
+    Battle.findOneAndUpdate(doc,
       doc, {
         new: true,
         upsert: true // Make this update into an upsert
